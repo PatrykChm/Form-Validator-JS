37a20fcd99c45bcafcdd876eb30203d9d88cc90b
Patryk <patrykchmielewskipl@gmail.com>
2022-11-07 14:53:29 +0100

    add clear inputs function

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ const sendBtn = document.querySelector('.send')
 const clearBtn = document.querySelector('.clear')
 const popup = document.querySelector('.popup')
 
+const clearInputs = input => {
+	input.forEach(el => {
+		el.value = ''
+	})
+}
+
+const showError = (input, msg) => {
+
+}
+
+
+
+
+
+sendBtn.addEventListener('click', (e)=>{
+	e.preventDefault()
+
+	
+})
+
+clearBtn.addEventListener('click', e => {
+	e.preventDefault()
+
+	clearInputs([username, pass, pass2, email])
+})
